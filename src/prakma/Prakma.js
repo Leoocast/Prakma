0e99caa8b463da7d3cf77bdebe905f902c85bc22
Leo Castellanos <leoocast@gmail.com>
2020-09-10 22:20:23 -0700

    Translate comments

--- src/prakma/Prakma.js
+++ src/prakma/Prakma.js
@@ -2,7 +2,9 @@ function Prakma(tag, attrs, ...children) {
     if (typeof tag === 'function') 
         return tag(attrs, children)
 
-    //Si el tag es object. O es un elemento, o es una funcin, entonces, o retornas o ejecutas
+    //If the tag is an object. Or is an element or is a function, so, or return it or run it.  
+
+    //Si el tag es object. O es un elemento, o es una función, entonces, o retornas o ejecutas.
     if (typeof tag === 'object')
         if (tag instanceof Element)
             return tag
@@ -15,14 +17,22 @@ function Prakma(tag, attrs, ...children) {
             }
         }
 
-    //Los tags normales de html sern strings para crear html
+    //The html common tags will be strings to create the element.     
+    
+    //Los tags normales de html seran strings para crear html.
     if (typeof tag === 'string') {
+        
+        //Fragments to add multiple children to the initial node.
+
         //Fragmentos para agregar multiples hijos al nodo inical
         const fragments = document.createDocumentFragment()
         const element = document.createElement(tag)
 
-        //Agregamos los atributos si es un botn o div, an no se pasa a global. En cuanto se controlen
-        //la mayora de los atributos esto ir fuera del if del button.
+        //Set the attributes if is a button or div, this isn't in the global scope yet.
+        //When I controlled all the attributes it will be.
+
+        //Agregamos los atributos si es un botón o div, aún no se pasa a global. En cuanto se controlen
+        //la mayoría de los atributos esto irá fuera del if del botón.
         if (tag === 'div' || tag === 'button') {
             if (attrs != null) {
                 for (item of Object.keys(attrs)) {
@@ -38,20 +48,28 @@ function Prakma(tag, attrs, ...children) {
             }
         }
 
-        //Si es un botn, le seteamos el click
+        //If is a button, set the click.
+
+        //Si es un botón, le seteamos el click
         if (tag === 'button') {
             if (attrs.onClick != undefined) {
                 element.addEventListener('click', attrs.onClick)
             }
 
         }
+
+        //If is a select, set onChange.
+
         //Si es un select, le seteamos el onChange
         if (tag === 'select')
             if (attrs.onChange != undefined)
                 element.addEventListener('change', attrs.onChange)
 
+        //If there are some styles, we need to time it out for create the element first. The other way
+        //it create the element and don't give the styles because doesn't exists.
+
         //Verificando si vienen algunos estilos, es necesario el time out 
-        //para que se cree primero el elemento, de otra manera se crea sin estilo despus de drselo        
+        //para que se cree primero el elemento, de otra manera se crea sin estilo después de dárselo.        
         if (attrs != null)
             if (attrs.style != undefined) {
                 setTimeout(() => {
@@ -66,25 +84,36 @@ function Prakma(tag, attrs, ...children) {
                 const textnode = document.createTextNode(child)
                 fragments.appendChild(textnode)
             }
-            // Cuando retornas un nmero, o map de nmeros
+            //When you return only a number or a map number.
+
+            // Cuando retornas un número, o map de números.
             else if (typeof child === 'number') {
                 const textnode = document.createTextNode(child)
                 fragments.appendChild(textnode)
             }
+            //If it is an object, it means that the tag function has something inside. Like:
+            //<Element><h1>There is something</h1></Element>.
+
             //Si es un objeto, quiere decir que el la funcin de tag tiene dentro algo
-            //<elemeto><h1>Aqui hay algo</h1></elemento>
+            //<Elemeto><h1>Aqui hay algo</h1></Elemento>.
             else if (typeof child === 'object') {
                 try {
                     child.forEach(r => fragments.appendChild(r))
                 } catch (error) {
                     child.forEach(r => {
+
+                        //If it's a string just create the node, otherwise loop each element.
+
                         //Si es string simplemente creas el nodo, si no, iteras sobre cada uno de los elementos
                         if (typeof r === 'string') {
                             const textnode = document.createTextNode(child)
                             fragments.appendChild(textnode)
                         } else {
                             r.forEach(x => {
-                                if (typeof x === 'string') { //Si es un texto combinado con elementos
+                                //If is a string merge with html elements.
+
+                                //Si es un string combinado con elementos html.
+                                if (typeof x === 'string') { 
                                     const textnode = document.createTextNode(x)
                                     fragments.appendChild(textnode)
                                 } else {
@@ -96,7 +125,10 @@ function Prakma(tag, attrs, ...children) {
                 }
             }
             else {
-                //Cualquier otra cosa que no sea un elemento html, string, number
+                
+                //Any thing that isn't a html element, object, function, string, number, etc.
+
+                //Cualquier otra cosa que no sea un elemento html, objecto, función, string, number, etc.
                 console.log('Not appendable', element)
                 console.log('Elements: ', tag, element, attrs, children)
             }
@@ -104,7 +136,9 @@ function Prakma(tag, attrs, ...children) {
 
         element.appendChild(fragments)
 
-        //Combinar el elemento con los atributos
+        //Merge the element with his attributes to return it.
+
+        //Combina el elemento con los atributos para retornarlo.
         Object.assign(element, attrs)
 
         return element
